505577fa279471c1904228be269d4dce1f92dc87
Erik Rodriguez <eric19.21.03@gmail.com>
2020-06-12 15:55:40 -0700

    Adds Binary Search Tree to names file

--- names/names.py
+++ names/names.py
@@ -10,7 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-# duplicates = [] Return the list of duplicates in this data structure
+duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
@@ -18,7 +18,55 @@ f.close()
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-duplicates = [name for name in names_2 if names_1.count(name)]
+# Stretch
+# duplicates = [name for name in names_2 if names_1.count(name)]
+
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        if target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+
+name = BSTNode("M")
+
+# first loop to add Binary search tree
+for names in names_1:
+    name.insert(names)
+
+# second loop to run condition on names and append to duplicates list
+for names in names_2:
+    if name.contains(names):
+        duplicates.append(names)
+
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
