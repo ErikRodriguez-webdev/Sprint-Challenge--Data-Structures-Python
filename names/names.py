488627e962ab7c461f7bb6a401592a6b1d9699e6
Erik Rodriguez <eric19.21.03@gmail.com>
2020-06-12 13:16:48 -0700

    Adds dupilcate names list comprehension to names file

--- names/names.py
+++ names/names.py
@@ -10,7 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# duplicates = [] Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
@@ -18,11 +18,7 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-   for name_1 in names_1:
-        pass
-
-    for name_2 in names_2:
-        pass
+duplicates = [name for name in names_2 if names_1.count(name)]
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
